2674590793b865b14d5fadf51c42680a21a0b13a
ye.wang <905317742@qq.com>
2024-12-31 16:45:56 +0800

    cpp实现detrend + butterworth

--- MicroHandGestureCollectorIWatch Watch App/cpp/detrend_iir/butterworth_filter.hpp
+++ MicroHandGestureCollectorIWatch Watch App/cpp/detrend_iir/butterworth_filter.hpp
@@ -9,12 +9,12 @@ class ButterworthFilter {
 public:
     ButterworthFilter(const std::vector<double>& b, const std::vector<double>& a);
     std::vector<double> filter(const std::vector<double>& x);
+    std::vector<double> detrend(const std::vector<double>& x);
 
 private:
     std::vector<double> b;
     std::vector<double> a;
     
-    std::vector<double> detrend(const std::vector<double>& x);
     std::pair<int, std::vector<double>> validatePad(const std::vector<double>& x);
     std::vector<double> lfilterZi();
     std::pair<std::vector<double>, std::vector<double>> lfilter(
