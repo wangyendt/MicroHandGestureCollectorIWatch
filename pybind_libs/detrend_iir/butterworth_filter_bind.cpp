954a4fdb326507b597259f1b51a1b9caf61ad51e
ye.wang <905317742@qq.com>
2024-12-31 18:32:27 +0800

    将python_lib移出xcode主目录下（否则会报错）

--- pybind_libs/detrend_iir/butterworth_filter_bind.cpp
+++ pybind_libs/detrend_iir/butterworth_filter_bind.cpp
@@ -5,8 +5,6 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(butterworth_filter, m) {
-    m.doc() = "Butterworth filter implementation"; // 可选的模块文档字符串
-
     py::class_<ButterworthFilter>(m, "ButterworthFilter")
         .def(py::init<const std::vector<double>&, const std::vector<double>&>())
         .def("filter", &ButterworthFilter::filter)
